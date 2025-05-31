fc60738f2b79be23aaf5c8848333e4a8cfe5eac4
Kirin Narine <kirin.narine@gmail.com>
2025-05-31 13:19:37 -0400

    incredibly basic char controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public float attackSpeed = 1f;
+
+    private Vector2 moveDirection = Vector2.zero;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        Debug.Log(moveX + ", " + moveY);
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
