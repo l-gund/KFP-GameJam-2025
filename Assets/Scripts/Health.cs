a7ff5f240965978548c4e5564030da5755f4d767
Kenneth Ho <8515878+kenesu-h@users.noreply.github.com>
2025-05-31 21:13:46 -0400

    Add basic fodder enemy with contact damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int maxHealth;
+
+    public void SetHealth(int amount)
+    {
+        health = amount;
+    }
+
+    public void SetMaxHealth(int amount)
+    {
+        maxHealth = amount;
+    }
+
+    public void Heal(int amount)
+    {
+        health = Mathf.Min(maxHealth, health + amount);
+    }
+
+    public void Damage(int amount)
+    {
+        health = Mathf.Max(0, health - amount);
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
